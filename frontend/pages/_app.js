d4dcc4a3558157f49403b844dbb79f407c38ab6d
unknown <cyberboy9971@gmail.com>
2022-10-12 03:26:39 +0530

    Add hooks to Card component

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,12 +2,42 @@ import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import EventCard from "../components/EventCard";
 import "../styles/globals.scss";
+import {useState} from 'react';
+
+
 
 function MyApp({ Component, pageProps }) {
+  
+  const [CardData, setCardData] = useState([
+    {
+      repoName: 'something',
+      tag: 'someTagString',
+      issueTitle: 'IssueTitle',
+      mentor: 'Mentor',
+      claim: false,
+      assignee: 'somedude'
+    },
+    {
+      repoName: 'something',
+      tag: 'someTagString',
+      issueTitle: 'IssueTitle',
+      mentor: 'Mentor',
+      claim: true,
+      assignee: 'somedude'
+    },
+    {
+      repoName: 'something',
+      tag: 'someTagString',
+      issueTitle: 'IssueTitle',
+      mentor: 'Mentor',
+      claim: false,
+      assignee: 'somedude'
+    }
+  ])
   return (
     <>
       <Navbar />
-      <Card />
+      <Card CardData={CardData}/>
       <EventCard />
       <Component {...pageProps} />
     </>
