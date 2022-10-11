a2b4b71070fe37e7523a5311a00a01be1351ee5d
Perseus-Jackson477 <vanshuppal2002@gmail.com>
2022-10-11 19:01:23 +0530

    add the usercard component for the about page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,8 @@
+import MemberCards from '../components/MemberCard'
 export default function Home() {
   return (
     <div className="center">
+      <MemberCards imgsource={'/ok.jpg'} name="urmom"/>
       <h2>Home Page</h2>
     </div>
   );
