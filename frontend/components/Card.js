0880e16aa8b76f646c8c669ea09bd002951d28ae
unknown <cyberboy9971@gmail.com>
2022-10-11 19:32:42 +0530

    Organise CSS

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,6 +1,4 @@
-import '../styles/Card.module.css'
-
-const Card = ({ somePropsPropertiesHere }) => {
+const Card = ({}) => {
     return (
     <div className="card">
         <div className="card__container">
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
