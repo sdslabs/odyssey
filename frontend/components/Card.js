7b2f3c08df3cf01a2b196c99c0126c716536c49e
unknown <cyberboy9971@gmail.com>
2022-10-11 17:26:20 +0530

    Fix font in rem

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -3,9 +3,10 @@ import '../styles/Card.module.css'
 const Card = ({ somePropsPropertiesHere }) => {
     return (
     <div className="card">
-        <div className="container">
+        <div className="card__container">
           <h4><b>Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet</b></h4>
           <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>
+          <button>Read More</button>
         </div>
     </div> 
     );
