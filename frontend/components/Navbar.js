0f386dd714160f280770ce9b1f77f574c3162de7
Aditya Bisht <adityabisht64@gmail.com>
2022-10-11 16:13:16 +0530

    Add Sass and Navbar Signin

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,16 +1,20 @@
 import Link from "next/link";
-import Image from "next/image";
 import React, { useState } from "react";
 import NavItem from "./NavItem";
+import { signIn, signOut, useSession, SessionProvider } from 'next-auth/react'
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
   { text: "About Us", href: "/about" },
   { text: "Contact", href: "/contact" },
+  { text: "Sign In", href: "/auth/signin" },
 ];
+
 const Navbar = () => {
   const [navActive, setNavActive] = useState(null);
   const [activeIdx, setActiveIdx] = useState(-1);
+
+  const { data: session, status } = useSession()
 
   return (
     <header>
@@ -40,6 +44,8 @@ const Navbar = () => {
               <NavItem active={activeIdx === idx} {...menu} />
             </div>
           ))}
+          {!session && <button onClick={() => signIn()}>Sign in with Github</button>}
+          {session && <button onClick={() => signOut()}>Sign out</button>}
         </div>
       </nav>
     </header>
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar";
-import "../styles/globals.css";
+import "../styles/globals.scss";
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
