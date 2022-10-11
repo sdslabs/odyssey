436bd9d96080e2ee2283da50769d5420ed2ab3a0
unknown <cyberboy9971@gmail.com>
2022-10-11 16:30:54 +0530

    Write Basic Card Component

---
+++ frontend/components/Card.js
@@ -0,0 +1,14 @@
+import '../styles/Card.module.css'
+
+const Card = ({ somePropsPropertiesHere }) => {
+    return (
+    <div className="card">
+        <div className="container">
+          <h4><b>Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet</b></h4>
+          <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>
+        </div>
+    </div> 
+    );
+  };
+
+export default Card;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
+import Card from "../components/Card";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <Card />
       <Component {...pageProps} />
     </>
   );
