932657b80fabb46d4df22f8346900b652b1ed51e
Aditya Bisht <adityabisht64@gmail.com>
2022-10-12 03:08:04 +0530

    Basic Landing Page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
-    <div className="center">
-      <h2>Home Page</h2>
+    <div className="placard">
+      <div>
+        <h2>Icons</h2>
+      </div>
+      <div className="placard-text">
+        <div>
+          <h1>Lorem ipsum dolor sit</h1>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>
+        </div>
+        <div>
+          <button>Lorem Ipsum</button>
+        </div>
+      </div>
+      <div>
+        <img src="/images/home-illus.svg" alt="Odyssey" />
+      </div>
     </div>
   );
 }
