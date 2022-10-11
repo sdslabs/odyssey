3ce542fc52379c25cad1da9d0459d42e261b09ba
Anshul Singh <anshulspidy@gmail.com>
2022-10-11 16:06:44 +0530

    Basic footer

---
+++ frontend/components/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div>
+            <p>Odyssey</p>
+        </div>
+        <div>
+            Home
+        </div>
+        <div>
+            <div>Social Media</div>
+            <div>Contact Us</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
