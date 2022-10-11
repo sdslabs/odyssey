42281ac84187b8850857b719fc95710725736ea4
jainl28patel <jainilpatel2003@gmail.com>
2022-10-11 22:31:44 +0530

    Add Search Bar Component

---
+++ frontend/components/Searchbar.js
@@ -0,0 +1,10 @@
+const Searchbar = ({}) => {
+    return (
+        <div className="searchbar">
+            <img src="images/searchicon.svg" className="searchIcon"></img>
+            <input className="searchText" placeholder="SEARCH"></input>
+        </div>
+    );
+};
+
+export default Searchbar;
