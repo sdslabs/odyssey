fe5060bbb5c303088107ea6a4bb68e8ae52f96e3
Pratham <prathamks.email@gmail.com>
2022-10-12 23:40:33 +0530

    import for testing

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,13 @@
 import HomePage from "../components/HomePage";
 import Timeline from "../components/Timeline";
+import Profile from "../components/Profile";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
       <Timeline />
+      <Profile />
     </div>
   );
 }
