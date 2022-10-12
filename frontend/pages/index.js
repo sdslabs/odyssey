1a4e6ba804f0f4e566af319a86e8d341777ee72e
Darshan Kumar <itsdarshankumar@gmail.com>
2022-10-12 18:22:10 +0530

    fix package-log

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,7 @@
-import Button from "../components/Button";
 export default function Home() {
   return (
     <div className="center">
       <h2>Home Page</h2>
-      <Button />
     </div>
   );
 }
