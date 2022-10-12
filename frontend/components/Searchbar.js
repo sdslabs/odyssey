c80f31058f3d473d4192fff087cf16724029b01b
Anshul Singh <anshulspidy@gmail.com>
2022-10-12 23:29:35 +0530

    Added route for testing

--- frontend/components/Searchbar.js
+++ frontend/components/Searchbar.js
@@ -1,7 +1,7 @@
 const Searchbar = ({}) => {
     return (
         <div className="searchbar">
-            <img src="images/searchicon.svg" className="searchIcon"></img>
+            <img src="/images/searchIcon.svg" className="searchIcon"></img>
             <input className="searchText" placeholder="SEARCH"></input>
         </div>
     );
