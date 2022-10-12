853b551def037cbbe093471f5655e76906bc2eed
Anshul Singh <anshulspidy@gmail.com>
2022-10-13 04:45:27 +0530

    Create route for testing

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,46 +1,12 @@
 import Navbar from "../components/Navbar";
-import Card from "../components/Card";
-import EventCard from "../components/EventCard";
 import Footer from "../components/Footer";
-import "../styles/globals.sscss";
-import {useState} from 'react';
-
-
+import "../styles/globals.scss";
 import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
-  
-  const [CardData, setCardData] = useState([
-    {
-      repoName: 'something',
-      tag: 'someTagString',
-      issueTitle: 'IssueTitle',
-      mentor: 'Mentor',
-      claim: false,
-      assignee: 'somedude'
-    },
-    {
-      repoName: 'something',
-      tag: 'someTagString',
-      issueTitle: 'IssueTitle',
-      mentor: 'Mentor',
-      claim: true,
-      assignee: 'somedude'
-    },
-    {
-      repoName: 'something',
-      tag: 'someTagString',
-      issueTitle: 'IssueTitle',
-      mentor: 'Mentor',
-      claim: false,
-      assignee: 'somedude'
-    }
-  ])
   return (
     <SessionProvider session={pageProps.session}>
       <Navbar />
-      <Card CardData={CardData}/>
-      <EventCard />
       <Component {...pageProps} />
       <Footer />
     </SessionProvider>
