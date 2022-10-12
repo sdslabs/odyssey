a8a78cb0a627542def40e221d31b536499c254e4
Anshul Singh <anshulspidy@gmail.com>
2022-10-13 03:16:40 +0530

    Adds route for testing

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,10 @@
 import Navbar from "../components/Navbar";
-import Card from "../components/Card";
-import EventCard from "../components/EventCard";
-import AboutCard from "../components/AboutCard";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <AboutCard />
       <Component {...pageProps} />
     </>
   );
