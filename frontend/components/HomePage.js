630673463edb70f3fd2f0a53b3ad1a0e1fc3e9f4
Darshan Kumar <itsdarshankumar@gmail.com>
2022-10-12 20:46:39 +0530

    minor landing fix

--- frontend/components/HomePage.js
+++ frontend/components/HomePage.js
@@ -1,25 +1,34 @@
 const HomePage = () => {
     return (
-        <div className="placard">
-            <div>
-                <h2>Icons</h2>
-            </div>
-            <div className="placard-text">
-                <div>
-                <h1>Lorem ipsum dolor sit</h1>
-                </div>
-                <div>
-                <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>
-                </div>
-                <div>
-                <button>Lorem Ipsum</button>
-                </div>
-            </div>
-            <div>
-                <img src="/images/home-illus.svg" alt="Odyssey" />
-            </div>
+      <div className="placard">
+        <div>
+          <h2>Icons</h2>
         </div>
+        <div className="placard-text">
+          <div>
+            <h1>Lorem ipsum dolor sit</h1>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum
+              dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit
+              ametLorem ipsum dolor sit amet
+            </p>
+          </div>
+          <div>
+            <button>Lorem Ipsum</button>
+          </div>
+        </div>
+        <div className="rightlane">
+          <div className="rightlogo">
+            <img src="/images/logo.svg" alt="logo" />
+          </div>
+          <div className="odysseyimage">
+            <img src="/images/home-illus.svg" alt="Odyssey" />
+          </div>
+        </div>
+      </div>
     );
 };
-
+    
 export default HomePage;
