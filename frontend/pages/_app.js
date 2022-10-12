623f392b31f5b59bdfee51fada3495b578ab2efc
Anshul Singh <anshulspidy@gmail.com>
2022-10-12 20:11:59 +0530

    Fix route and css

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,10 @@
 import Navbar from "../components/Navbar";
-import Card from "../components/Card";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <Card />
       <Component {...pageProps} />
     </>
   );
