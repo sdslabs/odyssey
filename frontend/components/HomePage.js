f91252a67306409d177ddabc4ce4816f94720e67
Anshul Singh <77113867+levihackerman-102@users.noreply.github.com>
2022-10-13 19:50:48 +0530

    Restructure css (#29)

--- frontend/components/HomePage.js
+++ frontend/components/HomePage.js
@@ -3,9 +3,10 @@ const HomePage = () => {
   return (
     <div className="placard">
       <div className="placard-icons">
-	  	<img className="placard-icons-instagram" src="/images/instagram2.svg" alt="instagram" />
-	    <img className="placard-icons-github" src="/images/github2.svg" alt="github" />	
-	  </div>
+        <img className="placard-icons-instagram" src="/images/instagram2.svg" alt="instagram" />
+        <img className="placard-icons-github" src="/images/github2.svg" alt="github" />	
+      </div>
+      
       <div className="placard-text">
         <div>
           <h1>Lorem ipsum dolor sit</h1>
