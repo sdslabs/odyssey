9f373e58058155c6ea69e1357c3b2f391d63d7d6
Darshan Kumar <itsdarshankumar@gmail.com>
2022-10-13 07:12:27 +0530

    fix button

--- frontend/components/Button.js
+++ frontend/components/Button.js
@@ -2,16 +2,14 @@ import React, { useState } from "react";
 
 const Button = () => {
   const [buttonActive, setButtonActive] = useState(false);
-  //const [btnActive, setBtnActive] = useState(false);
   const classNameGenerator = (...classes) => {
     return classes.join(" ");
   };
 
   return (
     <button
-      onClick ={() => setButtonActive(!buttonActive)}
-      //onMouseOut ={() => setButtonActive(!buttonActive)}
-      //onClick ={() => setBtnActive(!btnActive)}
+      onMouseDown={() => setButtonActive(!buttonActive)}
+      onMouseUp={() => setButtonActive(!buttonActive)}
       className={classNameGenerator("btn") + " " + buttonActive}
     >
       LOREM IPSUM
