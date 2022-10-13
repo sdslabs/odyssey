7d8dbd15f3674ef2a763aa991194e6d5f16a6a3f
Anshul Singh <anshulspidy@gmail.com>
2022-10-13 12:45:16 +0530

    Remove from index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,27 +1,12 @@
 import HomePage from "../components/HomePage";
 import Timeline from "../components/Timeline";
 
-import ReposToContribute from '../components/RepoList';
-const Repos=[
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum  "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "},
-  {mentor:"VANSH UPPAL", description:"Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum "}
-]
 export default function Home() {
 
   return (
-    <>    <div>
+    <div>
       <HomePage />
       <Timeline />
     </div>
-    <ReposToContribute list={Repos}/>
-    </>
   );
 }
