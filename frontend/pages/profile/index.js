5ae6dcded30616f022155d83eca6c241f00b68c4
Mayank Mittal <msquare.pvt21@gmail.com>
2022-10-18 03:36:30 +0530

    fix: profile page

--- frontend/pages/profile/index.js
+++ frontend/pages/profile/index.js
@@ -1,6 +1,8 @@
 import Profile from "../../components/Profile2";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+import UserProgress from "../../components/UserProgress";
+
 const axios = require("axios").default;
 
 export default function Home() {
@@ -8,27 +10,46 @@ export default function Home() {
   const [user, setUser] = useState(null);
 
   const fetchUserData = async () => {
-    const response = await axios.post('http://localhost:8000/api/get-user/', { access_token: session.accessToken, id_token: session.user.id });
-    return response
-  }
+    const response = await axios.post("http://localhost:8000/api/get-user/", {
+      access_token: session.accessToken,
+      id_token: session.user.id,
+    });
+    return response;
+  };
 
   useEffect(() => {
-    if (session) fetchUserData().then((response)=>{
-      let userData ={};
-      userData.uname = response.data.username;
-      userData.role = response.data.field;
-      userData.eno = response.data.enrollmentNo;
-      userData.contact = response.data.contactNo; 
-      userData.aname = response.data.name
-      userData.email = response.data.email
-      userData.pfp = session.user.image
-      setUser(userData)
-    });
+    if (session)
+      fetchUserData().then((response) => {
+        let userData = {};
+        userData.uname = response.data.username;
+        userData.role = response.data.field;
+        userData.eno = response.data.enrollmentNo;
+        userData.contact = response.data.contactNo;
+        userData.aname = response.data.name;
+        userData.email = response.data.email;
+        userData.pfp = session.user.image;
+        setUser(userData);
+      });
   }, [session]);
-  
+
   return (
-    <div>
-      {user ? <Profile uname={user.uname} aname={user.aname} role={user.role} eno={user.eno} contact={user.contact} email={user.email} pfp={user.pfp}/> : <div>loading</div>}
-    </div>
+    <>
+      {user ? (
+        <div className="profile-page-content">
+          <Profile
+            uname={user.uname}
+            aname={user.aname}
+            role={user.role}
+            eno={user.eno}
+            contact={user.contact}
+            email={user.email}
+            pfp={user.pfp}
+          />
+          <UserProgress progress={0} rank={"NA"} />
+        </div>
+      ) : (
+        <div>loading</div>
+      )}
+    </>
   );
 }
