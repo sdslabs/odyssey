d9aa8808705daf0f70b8a95d5c80a1dde1254277
Pratham <prathamks.email@gmail.com>
2022-10-17 17:11:56 +0530

    about page complete

--- frontend/pages/about/scrollable.js
+++ frontend/pages/about/scrollable.js
@@ -1,9 +1,38 @@
 import AboutTop from '../../components/AboutTop';
+import MemberCards from '../../components/MemberCard';
 
 export default function Home() {
   return (
     <div>
         <AboutTop />
+        <div className='about'>
+            <p>Designers</p>
+            <div className='members'>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            </div>
+        </div>
+        <div className='about'>
+            <p>Developers</p>
+            <div className='members'>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            </div>
+        </div>
+        <div className='about'>
+            <p>Mentors</p>
+            <div className='members'>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            <div className='member'><MemberCards imgsource={'/ok.jpg'} name="urmom"/></div>
+            </div>
+        </div>
     </div>
   );
 }
