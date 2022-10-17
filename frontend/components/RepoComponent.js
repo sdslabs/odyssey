0578448268754838f8b65c66338360fcbdf7144f
Mayank Mittal <msquare.pvt21@gmail.com>
2022-10-18 05:24:40 +0530

    fix: build

--- frontend/components/RepoComponent.js
+++ frontend/components/RepoComponent.js
@@ -16,7 +16,7 @@ const Repo = ({ Card }) => {
             {Card.repoName} - {Card.tag}
           </b>
         </h4>
-        <a href={Card.issueUrl} target="_blank">
+        <a href={Card.issueUrl} target="_blank" rel="noreferrer">
           {Card.issueTitle}
         </a>
       </div>
