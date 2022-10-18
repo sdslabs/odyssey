8ff8685764407a710a8eae2098ab5ea9e1435f9f
Perseus-Jackson477 <vanshuppal2002@gmail.com>
2022-10-18 18:26:20 +0530

    announcement scrolling complete

--- frontend/components/Announcements.js
+++ frontend/components/Announcements.js
@@ -1,31 +1,28 @@
 import EventCard from "./EventCard";
 import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
-const data=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] 
 
-const Announcement=({})=>{
+const Announcement=({data})=>{
   function slideLeft(){
     var slider=document.getElementById('events')
     if(!slider){
-      console.log("nai hua render")
       return
     }
-    slider.scrollLeft= slider.scrollLeft-444.23
+    slider.scrollLeft= slider.scrollLeft - ((document.getElementsByClassName("card")[0].scrollWidth+35)*3)
   }
    function slideRight(){
     var slider=document.getElementById('events')
     if(!slider){
-      console.log("nai hua render")
       return
     }
-    slider.scrollLeft= slider.scrollLeft+444.23
+    slider.scrollLeft= slider.scrollLeft + ((document.getElementsByClassName("card")[0].scrollWidth+35)*3)
   }
   return(
     <div className="announcement">
       <h1>Announcement</h1>     
 
       <div className='anouncebox'>
-      <MdChevronLeft onClick={()=>slideLeft()} size={480}/>
+      <MdChevronLeft onClick={()=>slideLeft()} size={200}/>
       <div className="events" id="events">
       {data.map((item)=>{
         return(
@@ -36,9 +33,8 @@ const Announcement=({})=>{
         description={item}/>);
       })}
       </div>
-      <MdChevronRight onClick={()=>slideRight()} size={480}/>
+      <MdChevronRight onClick={()=>slideRight()} size={200}/>
       </div>
-
     </div>
   );
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,14 @@
 import HomePage from "../components/HomePage";
 import Timeline from "../components/Timeline";
 import Announcement from "../components/Announcements"
+const data=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] 
 
 export default function Home() {
   return (
     <div>
       <HomePage />
       <Timeline />
-      <Announcement/>
+      <Announcement data={data}/>
     </div>
   );
 }
